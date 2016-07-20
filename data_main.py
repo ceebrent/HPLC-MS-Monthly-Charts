56097622e7630251e2ca799e0cd1c9b6d7794d27
Brent <cbrent0121@gmail.com>
2016-07-20 06:28:26 -0400

    begin modifying methods to exclude dates and include machine names

--- data_main.py
+++ data_main.py
@@ -14,51 +14,50 @@ import pandas as pd
 
 
 class LeveyJennings(object):
-    def __init__(self, lab_name):
+    def __init__(self):
         self.homeDirectory = get_home()
-        self.lab_name = lab_name
 
     # Creates and returns folder to store results into
         def results_folder(self):
             # move_directory_up = Path(self.homeDirectory).parents[0]
             
-            result_folder = os.path.join(self.homeDirectory,'Results')
+            result_folder = os.path.join(self.homeDirectory, 'Results')
             os.makedirs(result_folder, exist_ok=True)
             
-            lab_results = os.path.join(result_folder, self.lab_name)
+            machine_results = os.path.join(result_folder, 'Machine Results')
 
-            os.makedirs(lab_results, exist_ok=True)
-            return lab_results
-        self.lab_results = results_folder(self)
+            os.makedirs(machine_results, exist_ok=True)
+            return machine_results
+        self.machine_results = results_folder(self)
 
     # Walks through all files and directories in home folder with ending text and containing lab name
         def original_txt(self):
             lab_text_files = []
             for dirpath, dirnames, files in os.walk(os.path.join(self.homeDirectory, 'Data')):
                 for filename in files:
-                    if filename.endswith('.txt') and self.lab_name in filename:
+                    if filename.endswith('.txt') in filename:
                         lab_text_files.append(os.path.join(dirpath, filename))
             return lab_text_files
-        self.lab_text_files = original_txt(self)
+        self.text_files = original_txt(self)
 
     def make_unique_files(self):
         # Copies all appropriate files into result folder and takes newest version of file
-        new_result_path = os.path.join(self.lab_results, 'Temp_TXT')
+        new_result_path = os.path.join(self.machine_results, 'Temp_TXT')
         os.makedirs(new_result_path, exist_ok=True)
 
-        for x in range(len(self.lab_text_files)):
+        for x in range(len(self.text_files)):
             # Put original files into TXT folder, check for duplicates
-            file_name = os.path.join(new_result_path, os.path.basename(self.lab_text_files[x]))
+            file_name = os.path.join(new_result_path, os.path.basename(self.text_files[x]))
             silent_remove(file_name)
-            shutil.copy2(self.lab_text_files[x], new_result_path)
+            shutil.copy2(self.text_files[x], new_result_path)
 
             # Create new name in "Date Method Batch Lab format
-            date = get_date(self.lab_text_files[x])
-            old_file_name = file_name_regex(os.path.basename(self.lab_text_files[x]))
+            date = get_date_machine(self.text_files[x])
+            old_file_name = file_name_regex(os.path.basename(self.text_files[x]))
             unique_file_name = "{date} {old_file} {lab_name}.txt".format(date=date, old_file=old_file_name,
                                                                          lab_name=self.lab_name)
 
-            unique_path = os.path.join(self.lab_results, os.path.basename(unique_file_name))
+            unique_path = os.path.join(self.machine_results, os.path.basename(unique_file_name))
             silent_remove(unique_path)
             os.rename(file_name, unique_path)
 
@@ -67,36 +66,32 @@ def make_month_folders(result_path):
     list_of_files = glob.glob(result_path+'\\*.txt')
     for file in list_of_files:
         file_name = os.path.basename(file)
-        month_digits = file_name[4:6]
-        if month_digits.startswith('0') or month_digits.startswith('1'):
-            if month_digits.startswith('0'):
-                month_digits = month_digits[1]
-            try:
-                month_name = calendar.month_name[int(month_digits)]
-            except IndexError:
-                raise SystemExit("Index Error")
+        machine_name = file_name[:4]
 
-            year = file_name[:4]
-            month_folder_name = '{month_name} {year}'.format(
-                month_name=month_name, year=year
-            )
-            month_folder = os.path.join(result_path, month_folder_name)
-            os.makedirs(month_folder, exist_ok=True)
-            # Move into months folders
-            try:
-                shutil.move(file, month_folder)
-            # If file exists in month folder, delete it and add new
-            except shutil.Error:
-                os.remove(os.path.join(month_folder, os.path.basename(file)))
-                shutil.move(file, month_folder)
+        machine_folder = os.path.join(result_path, machine_name)
+        os.makedirs(machine_folder, exist_ok=True)
+        # Move into months folders
+        try:
+            shutil.move(file, machine_folder)
+        # If file exists in month folder, delete it and add new
+        except shutil.Error:
+            os.remove(os.path.join(machine_folder, os.path.basename(file)))
+            shutil.move(file, machine_folder)
 
 
 # Gets date file was created from cell in original text file
-def get_date(text_file):
+def get_date_machine(text_file):
     with open(text_file, 'r') as original_file:
         row = original_file.readlines()[1].split('\t')
-        file_name = os.path.basename(row[2])[:8]
-        return file_name
+        date_text = os.path.basename(row[2])[:8]
+        machine_row = os.path.normpath(row[2]).split(os.sep)
+        machine_name = ''
+        if 'Projects' in machine_row:
+            project_index = machine_row.index('Projects')
+            machine_path = machine_row[project_index + 1]
+            machine_name = machine_path[:4]
+        name_date = '{machine_name} {date}'.format(machine_name=machine_name, date=date_text)
+        return name_date
 
 
 # Gets Unique portion of original file name to append to date file
@@ -159,10 +154,11 @@ def get_home():
         return os.path.dirname(sys.argv[0])
 
 
-def generate_data(lab_value):
-    lab = LeveyJennings(lab_value)
+def generate_data():
+    lab = LeveyJennings()
     lab.make_unique_files()
-    make_month_folders(lab.lab_results)
-    walk_months(lab.lab_results)
+    make_month_folders(lab.machine_results)
+    walk_months(lab.machine_results)
 
 
+generate_data()
