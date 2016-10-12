3a681a7aabc223c30b72d8cce9d8d2825db6f7b1
MassSpec Conversion <MassSpec@completetox.local>
2016-10-12 14:46:32 -0400

    add hook for openpyxl version

--- graph_package.py
+++ graph_package.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.backends.backend_pdf import PdfPages
+import openpyxl
 from openpyxl import load_workbook
 import errno
 import xlsxwriter
